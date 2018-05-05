0f9b0f1263f83038d68fe5dc09de9830a963f1ce
Sean Clifford <sclifford6262@gmail.com>
2018-05-05 18:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function(drivers){
-  return [drivers[0], drivers[1]]
+  return drivers.slice(0, 2)
 }
 
 const returnLastTwoDrivers = function(drivers){
