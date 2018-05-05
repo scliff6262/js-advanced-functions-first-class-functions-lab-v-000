411b250b9aa23f1448d1278d67499a041070f1c0
Sean Clifford <sclifford6262@gmail.com>
2018-05-05 18:05:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const returnLastTwoDrivers = function(drivers){
   return drivers.slice(-2)
 }
 
-const = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
